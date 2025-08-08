22d3f148316db61973d521cc6a1fa38a62388592
Nareshkumart19 <kumarnaresht19@gmail.com>
2025-08-08 15:32:10 +0530

    docker -ec2

--- docker.sh
+++ docker.sh
@@ -15,6 +15,8 @@ xfs_growfs /
 xfs_growfs /var
 
 
+# eks install 
+
 ARCH="amd64"
 PLATFORM="$(uname -s)_$ARCH"
 
@@ -41,3 +43,27 @@ sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
 echo "✅ eksctl version installed:"
 eksctl versio
 
+### kublet 
+
+set -e  # Exit on error
+
+# Configurable variables
+VERSION="1.33.0"
+RELEASE_DATE="2025-05-01"
+ARCH="amd64"
+OS="linux"
+BIN_PATH="/usr/local/bin"
+DOWNLOAD_URL="https://s3.us-west-2.amazonaws.com/amazon-eks/${VERSION}/${RELEASE_DATE}/bin/${OS}/${ARCH}/kubectl"
+TMP_FILE="./kubectl"
+
+echo "📥 Downloading kubectl v$VERSION for $OS/$ARCH..."
+curl -O "$DOWNLOAD_URL"
+
+echo "🔧 Setting executable permission..."
+chmod +x "$TMP_FILE"
+
+echo "📦 Moving kubectl to $BIN_PATH..."
+sudo mv "$TMP_FILE" "$BIN_PATH/kubectl"
+
+echo "✅ kubectl installed. Verifying version:"
+kubectl version --client
