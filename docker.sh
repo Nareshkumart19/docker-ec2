02f6932720fdd14167d1c2c7250a432d29f277ce
Nareshkumart19 <kumarnaresht19@gmail.com>
2025-08-09 09:39:04 +0530

    docker

--- docker.sh
+++ docker.sh
@@ -15,55 +15,18 @@ xfs_growfs /
 xfs_growfs /var
 
 
-# eks install 
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
 
-ARCH="amd64"
-PLATFORM="$(uname -s)_$ARCH"
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.0/2025-05-01/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv kubectl /usr/local/bin/kubectl
 
-echo "Downloading eksctl for $PLATFORM..."
+eksctl version
+kubectl version
 
-# Download eksctl tarball
-curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_${PLATFORM}.tar.gz"
-
-# Verify checksum
-echo "Verifying checksum..."
-curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" \
-  | grep "$PLATFORM" \
-  | sha256sum --check || {
-    echo "❌ Checksum verification failed!"
-    exit 1
-  }
-
-# Extract and install
-echo "Extracting and installing eksctl..."
-tar -xzf eksctl_"${PLATFORM}".tar.gz -C /tmp && rm eksctl_"${PLATFORM}".tar.gz
-sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
-
-# Confirm installation
-echo "✅ eksctl version installed:"
-eksctl versio
-
-### kublet 
-
-set -e  # Exit on error
-
-# Configurable variables
-VERSION="1.33.0"
-RELEASE_DATE="2025-05-01"
-ARCH="amd64"
-OS="linux"
-BIN_PATH="/usr/local/bin"
-DOWNLOAD_URL="https://s3.us-west-2.amazonaws.com/amazon-eks/${VERSION}/${RELEASE_DATE}/bin/${OS}/${ARCH}/kubectl"
-TMP_FILE="./kubectl"
-
-echo "📥 Downloading kubectl v$VERSION for $OS/$ARCH..."
-curl -O "$DOWNLOAD_URL"
-
-echo "🔧 Setting executable permission..."
-chmod +x "$TMP_FILE"
-
-echo "📦 Moving kubectl to $BIN_PATH..."
-sudo mv "$TMP_FILE" "$BIN_PATH/kubectl"
-
-echo "✅ kubectl installed. Verifying version:"
-kubectl version --client
+git clone https://github.com/ahmetb/kubectx /opt/kubectx
+ln -s /opt/kubectx/kubens /usr/local/bin/kubens
